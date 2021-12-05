7f8328b95d098b005847231d9e48f79fb5bfaf0a
RAMAROSON Avonasandratra Faustin <avonasandratra2003@gmail.com>
2021-12-05 16:25:37 +0300

    design the side clientele service

--- config/helper.js
+++ config/helper.js
@@ -7,7 +7,13 @@ const menu = (matricule) => {
             {link: "/produits/0", content: "Produits", icons: "fa fa-cubes", active: "produits"},
             {link: "/etatVente", content: "Etat de vente", icons: "fa fa-money", active: "money"},
         ];
-        const scMenu = [];
+        const scMenu = [
+            {link: "/", content: "Accueil", icons: "fa fa-home", active: "home"},
+            {link: "/clients/0", content: "Clients", icons: "fa fa-users", active: "clients"},
+            {link: "/livraisons", content: "Tache", icons: "fa fa-car", active: "livraison"},
+            {link: "/produits/0", content: "Produits", icons: "fa fa-cubes", active: "produits"},
+            {link: "/etatLivraison", content: "Etat de Livraison", icons: "fa fa-truck", active: "money"},
+        ];
         return matricule[0] + matricule[1] === "VB" ? vbMenu : scMenu;
     }
     return [];
